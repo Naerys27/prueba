8926322dff554b3bfea642e69b90cbd74763a3ad
Naerys27 <david.amboades@gmail.com>
2026-05-14 11:59:49 +0200

    Cambio de PDF

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-const CACHE = 'partes-loco-v52';
+const CACHE = 'partes-loco-v56';
 const FILES = [
   'index.html',
   'parte_combustible.html',
+  'parte_combustible_v2.html',
   'parte_servicio_diario.html',
+  'parte_servicio_diario_v2.html',
   'orden_reparacion.html',
+  'orden_reparacion_v2.html',
   'storage.js',
   'manifest.json',
   'icons/icon-192.png',
